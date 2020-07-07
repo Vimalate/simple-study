cf814dec8f0b420c7923327de04061cdad16e287
“Vimalate <1902644369@qq.com>
2020-07-07 14:11:10 +0800

    vue-typescript 便签

---
+++ typescript-vue/src/App.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Author: Vimalakirti
+ * @Date: 2020-06-12 21:36:27
+ * @LastEditTime: 2020-06-12 23:15:57
+ * @Description: 
+ * @FilePath: \简单实现\typescript-vue\src\App.vue
+--> 
+<template>
+  <div id="app">
+   <menu-bar></menu-bar>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import MenuBar from './components/MenuBar.vue'
+@Component({
+  components: {
+    MenuBar
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
