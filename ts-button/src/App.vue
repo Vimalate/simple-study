6af0fd58f842268dd63fe5ce6ef7ce8881a58912
“Vimalate <1902644369@qq.com>
2020-07-07 14:24:33 +0800

    ts-button init

---
+++ ts-button/src/App.vue
@@ -0,0 +1,33 @@
+<!--
+ * @Author: Vimalakirti
+ * @Date: 2020-07-07 14:21:33
+ * @LastEditTime: 2020-07-07 14:23:46
+ * @Description: 
+ * @FilePath: \简单实现\ts-button\src\App.vue
+--> 
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {
+    
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
